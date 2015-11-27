a2e8ab3420e8a8e6190b1d2b4b3483f346a3cd51
Daniel Maier <dm@bergwerk.ag>
2015-11-27 15:02:16 +0100

    init extension

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'BERGWERK Address',
+	'description' => 'Manage addresses, display a list of all addresses, define a custom order and display detail views of every address.',
+	'category' => 'plugin',
+	'version' => '0.1.0',
+	'state' => 'alpha',
+	'author' => 'Daniel Maier',
+	'author_email' => 'dm@bergwerk.ag',
+	'author_company' => 'BERGWERK',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99'
+		),
+		'conflicts' => array(),
+		'suggests' => array()
+	)
+);
