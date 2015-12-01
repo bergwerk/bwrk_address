7d8979f67e0b04b02ceb3c3ab6d365cf10931290
Daniel Maier <dm@bergwerk.ag>
2015-12-01 09:50:53 +0100

    [task] cleanup

--- Configuration/TCA/tx_bwrkaddress_domain_model_address.php
+++ Configuration/TCA/tx_bwrkaddress_domain_model_address.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:bwrk_utility/Classes/Utility/Tca/Configuration.php');
-require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:bwrk_utility/Classes/Utility/Tca/AbstractTca.php');
-require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:bwrk_utility/Classes/Utility/Tca/Tca.php');
-require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:bwrk_address/Classes/Configuration/Tca/AddressTca.php');
-
 $tcaConfiguration = new \BERGWERK\BwrkUtility\Utility\Tca\Configuration();
 $tcaConfiguration->setExt(\BERGWERK\BwrkAddress\Bootstrap::$_extKey);
 $tcaConfiguration->setModel('tx_bwrkaddress_domain_model_address');
--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,4 @@
 
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
-require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:bwrk_address/Classes/Bootstrap.php');
-
 \BERGWERK\BwrkAddress\Bootstrap::extTables();
