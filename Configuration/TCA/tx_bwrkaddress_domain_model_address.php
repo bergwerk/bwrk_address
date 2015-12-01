b129ea44dcc2ef3794a450c20e1f042e65f871d5
Daniel Maier <dm@bergwerk.ag>
2015-12-01 10:40:28 +0100

    [task] change icons to be svgs

--- Configuration/TCA/tx_bwrkaddress_domain_model_address.php
+++ Configuration/TCA/tx_bwrkaddress_domain_model_address.php
@@ -4,14 +4,16 @@ $tcaConfiguration = new \BERGWERK\BwrkUtility\Utility\Tca\Configuration();
 $tcaConfiguration->setExt(\BERGWERK\BwrkAddress\Bootstrap::$_extKey);
 $tcaConfiguration->setModel('tx_bwrkaddress_domain_model_address');
 $tcaConfiguration->setLabelField('title');
-$tcaConfiguration->setIconFile('EXT:bwrk_address/Resources/Public/Icons/Address.png');
+$tcaConfiguration->setIconFile('EXT:bwrk_address/Resources/Public/Icons/Address.svg');
 
 $tca = new \BERGWERK\BwrkUtility\Utility\Tca\Tca();
 $tca->init($tcaConfiguration);
 
-$tca->addInputField('title', 'LLL:EXT:bwrk_address/Resources/Private/Language/locallang_db.xlf:title');
+$tca->addInputField('title');
 
-$tca->addTab('tab_references', 'LLL:EXT:bwrk_address/Resources/Private/Language/locallang_db.xlf:references');
+$tca->addReferenceField('entries', 'tx_bwrkaddress_domain_model_address_entry', 'address', 'sorting');
+
+$tca->addTab('tab_references');
 $tca->addSysCategoryReferences('categories', 0, 10, 0, 999, 'LLL:EXT:bwrk_address/Resources/Private/Language/locallang_db.xlf:categories');
 
 return $tca->createTca();
--- Configuration/TCA/tx_bwrkaddress_domain_model_address_entry.php
+++ Configuration/TCA/tx_bwrkaddress_domain_model_address_entry.php
@@ -4,12 +4,12 @@ $tcaConfiguration = new \BERGWERK\BwrkUtility\Utility\Tca\Configuration();
 $tcaConfiguration->setExt(\BERGWERK\BwrkAddress\Bootstrap::$_extKey);
 $tcaConfiguration->setModel('tx_bwrkaddress_domain_model_address_entry');
 $tcaConfiguration->setLabelField('entry_value');
-$tcaConfiguration->setIconFile('EXT:bwrk_address/Resources/Public/Icons/AddressEntry.png');
+$tcaConfiguration->setIconFile('EXT:bwrk_address/Resources/Public/Icons/AddressEntry.svg');
 
 $tca = new \BERGWERK\BwrkUtility\Utility\Tca\Tca();
 $tca->init($tcaConfiguration);
 
-$tca->addInputField('entry_key', 'Key');
+$tca->addInputField('entry_type', 'Type');
 $tca->addInputField('entry_value', 'Value');
 
 return $tca->createTca();
