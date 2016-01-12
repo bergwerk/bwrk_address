df5d427c032fe790d6aa2aba7bc6ad0a0c1e5cb6
Georg Dümmler <gd@bergwerk.ag>
2016-01-12 09:05:01 +0100

    add ext_autoload.php for namespace requirements

---
+++ ext_autoload.php
@@ -0,0 +1,6 @@
+<?php
+
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('bwrk_address');
+return array(
+    'BERGWERK\BwrkAddress\Bootstrap' => $extensionPath . 'Classes/Bootstrap.php',
+);
